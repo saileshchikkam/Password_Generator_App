e4639c2dd2df221afe15d95cf61f13041f736531
sailesh chikkam <sailesh.ch118@gmail.com>
2025-10-30 10:55:17 +0530

    Add files via upload

---
+++ database_of_HLgame.py
@@ -0,0 +1,141 @@
+data = [
+    {
+        'name': "sailesh",
+        'follower_count': 3,
+        'description': "AIML Student",
+        'country': "India"
+    },
+    {
+        'name': "Virat Kohli",
+        'follower_count': 270,
+        'description': "Cricketer",
+        'country': "India"
+    },
+    {
+        'name': "MS Dhoni",
+        'follower_count': 45,
+        'description': "Former Indian Cricket Captain",
+        'country': "India"
+    },
+    {
+        'name': "Elon Musk",
+        'follower_count': 180,
+        'description': "CEO of Tesla and SpaceX",
+        'country': "USA"
+    },
+    {
+        'name': "Cristiano Ronaldo",
+        'follower_count': 620,
+        'description': "Footballer",
+        'country': "Portugal"
+    },
+    {
+        'name': "Lionel Messi",
+        'follower_count': 480,
+        'description': "Footballer",
+        'country': "Argentina"
+    },
+    {
+        'name': "Narendra Modi",
+        'follower_count': 95,
+        'description': "Prime Minister of India",
+        'country': "India"
+    },
+    {
+        'name': "Taylor Swift",
+        'follower_count': 290,
+        'description': "Singer and Songwriter",
+        'country': "USA"
+    },
+    {
+        'name': "Selena Gomez",
+        'follower_count': 430,
+        'description': "Singer and Actress",
+        'country': "USA"
+    },
+    {
+        'name': "Dwayne Johnson",
+        'follower_count': 410,
+        'description': "Actor and Former Wrestler",
+        'country': "USA"
+    },
+    {
+        'name': "Kylie Jenner",
+        'follower_count': 400,
+        'description': "Model and Businesswoman",
+        'country': "USA"
+    },
+    {
+        'name': "Emma Watson",
+        'follower_count': 80,
+        'description': "Actress and Activist",
+        'country': "UK"
+    },
+    {
+        'name': "Bill Gates",
+        'follower_count': 45,
+        'description': "Philanthropist and Microsoft Founder",
+        'country': "USA"
+    },
+    {
+        'name': "Sundar Pichai",
+        'follower_count': 8,
+        'description': "CEO of Google",
+        'country': "India"
+    },
+    {
+        'name': "Jeff Bezos",
+        'follower_count': 12,
+        'description': "Founder of Amazon",
+        'country': "USA"
+    },
+    {
+        'name': "Ariana Grande",
+        'follower_count': 380,
+        'description': "Singer and Actress",
+        'country': "USA"
+    },
+    {
+        'name': "Shah Rukh Khan",
+        'follower_count': 45,
+        'description': "Actor",
+        'country': "India"
+    },
+    {
+        'name': "Robert Downey Jr.",
+        'follower_count': 65,
+        'description': "Actor (Iron Man)",
+        'country': "USA"
+    },
+    {
+        'name': "LeBron James",
+        'follower_count': 160,
+        'description': "Basketball Player",
+        'country': "USA"
+    },
+    {
+        'name': "Sailesh Chikkam",
+        'follower_count': 3,
+        'description': "AIML Student",
+        'country': "India"
+    },
+    {
+        'name': "Mark Zuckerberg",
+        'follower_count': 12,
+        'description': "Founder of Facebook",
+        'country': "USA"
+    }
+]
+logo = ('''
+  _  _ _      _              _                          ___                
+ | || (_)__ _| |_  ___ _ _  | |   _____ __ _____ _ _   / __|__ _ _ __  ___ 
+ | __ | / _` | ' \/ -_) '_| | |__/ _ \ V  V / -_) '_| | (_ / _` | '  \/ -_)
+ |_||_|_\__, |_||_\___|_|   |____\___/\_/\_/\___|_|    \___\__,_|_|_|_\___|
+        |___/                                                              
+''')
+
+vs = ('''
+ __ _____
+ \ V (_-<
+  \_//__/       
+''')
